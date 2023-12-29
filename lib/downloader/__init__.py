dbc2de2ec14980d57aa4a56b877352f33feedbb7
Ravindu Dilshan <90592440+ravindudil5han@users.noreply.github.com>
2023-12-29 17:47:00 +0530

    Update

--- lib/downloader/__init__.py
+++ lib/downloader/__init__.py
@@ -9,11 +9,11 @@ __license__ = "The Unlicense (Unlicense)"
 __js__ = None
 __js_url__ = None
 
-from pytube.version import __version__
-from pytube.streams import Stream
-from pytube.captions import Caption
-from pytube.query import CaptionQuery, StreamQuery
-from pytube.__main__ import YouTube
-from pytube.contrib.playlist import Playlist
-from pytube.contrib.channel import Channel
-from pytube.contrib.search import Search
+from downloader.version import __version__
+from downloader.streams import Stream
+from downloader.captions import Caption
+from downloader.query import CaptionQuery, StreamQuery
+from downloader.__main__ import YouTube
+from downloader.contrib.playlist import Playlist
+from downloader.contrib.channel import Channel
+from downloader.contrib.search import Search
